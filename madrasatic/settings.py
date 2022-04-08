1870ae91c176a8a9833147d157362787838bfc98
ikramysf <gm_yousfi@esi.dz>
2022-04-08 06:34:47 +0200

    accept

--- madrasatic/settings.py
+++ madrasatic/settings.py
@@ -83,10 +83,13 @@ WSGI_APPLICATION = 'madrasatic.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-  }
-
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'madrasatic',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
 }
 
 
